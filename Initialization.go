3e83cef5dcfbe1d4f231a0675f0ac265b062e354
sk4x0r <santosh.patil123@gmail.com>
2014-02-01 02:09:41 +0530

    Initial commit

--- Initialization.go
+++ Initialization.go
@@ -1,58 +1,57 @@
 package cluster
 
-import(
+import (
 	"fmt"
 	//"os"
-	"io/ioutil"
 	"encoding/json"
 	zmq "github.com/pebbe/zmq4"
+	"io/ioutil"
 	"strconv"
 )
 
-
-func speak(s string){
+func speak(s string) {
 	fmt.Println(s)
 }
 
-func parseConfigFile(configFile string) (Config){
+func parseConfigFile(configFile string) Config {
 	//speak("Inside parseConfigFile()")
 	content, err := ioutil.ReadFile(configFile)
-	if err!=nil{
+	if err != nil {
 		fmt.Println("Error parsing the config file")
 		panic(err)
 	}
 	var conf Config
-	err=json.Unmarshal(content, &conf)
-	if err!=nil{
+	err = json.Unmarshal(content, &conf)
+	if err != nil {
 		fmt.Println("Error parsing the config file")
 		panic(err)
 	}
 	return conf
 }
 
-func loadServer(serverId int, conf Config)(Server){
+func loadServer(serverId int, conf Config) Server {
 	//speak("Inside loadServer()")
 	var s Server
-	s.pid=serverId
-	
-	s.peers=conf.getPeers(serverId)
-	s.inbox=make(chan *Envelope,100)
-	s.outbox=make(chan *Envelope,100)
-	s.port=conf.getPort(serverId)
-	s.peerInfo=conf.getPeerInfo(serverId)
-	
+	s.pid = serverId
+
+	s.peers = conf.getPeers(serverId)
+	s.inbox = make(chan *Envelope, 100)
+	s.outbox = make(chan *Envelope, 100)
+	s.port = conf.getPort(serverId)
+	s.peerInfo = conf.getPeerInfo(serverId)
+
 	//create sockets for each peer
-	s.connections=make(map[int] *zmq.Socket)
-	for i:=range s.peers{
-		peerId:=s.peers[i]
-		
-		sock, err :=zmq.NewSocket(zmq.REQ)
-		if err!=nil{
-			fmt.Println("Error creating socket",err)
+	s.connections = make(map[int]*zmq.Socket)
+	for i := range s.peers {
+		peerId := s.peers[i]
+
+		sock, err := zmq.NewSocket(zmq.REQ)
+		if err != nil {
+			fmt.Println("Error creating socket", err)
 		}
-		st:="tcp://"+s.peerInfo[peerId].Ip+":"+strconv.Itoa(s.peerInfo[peerId].Port)
+		st := "tcp://" + s.peerInfo[peerId].Ip + ":" + strconv.Itoa(s.peerInfo[peerId].Port)
 		sock.Connect(st)
-		s.connections[peerId]=sock
+		s.connections[peerId] = sock
 		//s.peerInfo[peerId].soc= s.connections[peerId]
 	}
 	//fmt.Println(s.peerInfo)
@@ -61,9 +60,9 @@ func loadServer(serverId int, conf Config)(Server){
 	go s.handleOutbox()
 	return s
 }
-func New(serverId int, configFile string) Server{
+func New(serverId int, configFile string) Server {
 	//speak("Inside New()")
-	conf:= parseConfigFile(configFile)
-	s:=loadServer(serverId, conf)
+	conf := parseConfigFile(configFile)
+	s := loadServer(serverId, conf)
 	return s
 }
--- cluster_test.go
+++ cluster_test.go
@@ -1,46 +1,47 @@
 package cluster
 
 import (
-        //"flag"
-        "fmt"
-        "time"
-        "testing"
-        //"github.com/sk4x0r/cluster"
-)
-var(
-	PATH_TO_CONFIG="config.json"
+	//"flag"
+	"fmt"
+	"testing"
+	"time"
+	//"github.com/sk4x0r/cluster"
 )
 
-func createDummyMessage(msgId int, pid int) Envelope{
-	e:=Envelope{Pid:pid, MsgId:int64(msgId), Msg:"Dummy Message"}
+var (
+	PATH_TO_CONFIG = "config.json"
+)
+
+func createDummyMessage(msgId int, pid int) Envelope {
+	e := Envelope{Pid: pid, MsgId: int64(msgId), Msg: "Dummy Message"}
 	return e
 }
 
-func sendMessages(s Server, count int, pid int){
-	outbox:=s.Outbox()
-	for i:=0;i<count;i++ {
-		msg:=createDummyMessage(i,pid)
+func sendMessages(s Server, count int, pid int) {
+	outbox := s.Outbox()
+	for i := 0; i < count; i++ {
+		msg := createDummyMessage(i, pid)
 		outbox <- &msg
-		time.Sleep(20*time.Millisecond)
+		time.Sleep(20 * time.Millisecond)
 	}
 	fmt.Println("Sent ", count, " messages to ", pid)
 }
 
-func receiveMessages(s Server, count int){
-	inbox:=s.Inbox()
-	for i:=0;i<count;i++{
+func receiveMessages(s Server, count int) {
+	inbox := s.Inbox()
+	for i := 0; i < count; i++ {
 		//fmt.Println("Received",i)
-		<- inbox
+		<-inbox
 	}
 	fmt.Println("Received ", count, "messages")
 }
 
 func TestSendReceive(t *testing.T) {
-	sender:=New(1001, PATH_TO_CONFIG)
-	receiver:=New(1002, PATH_TO_CONFIG)
-	
-	msgCount:=10
+	sender := New(1001, PATH_TO_CONFIG)
+	receiver := New(1002, PATH_TO_CONFIG)
+
+	msgCount := 10
 	go sendMessages(sender, msgCount, receiver.Pid())
 	go receiveMessages(receiver, msgCount)
-	time.Sleep(15*time.Second)
+	time.Sleep(15 * time.Second)
 }
